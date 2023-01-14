fd1d08fa0ce60f4dccf3a23544b3e4c4af70bfcd
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-01-14 06:51:53 +0000

    topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :homes, :books
+  resources :books,only: [:create, :index, :show, :destroy, :edit, :update]
+  root to: 'homes#top'
 
 end
