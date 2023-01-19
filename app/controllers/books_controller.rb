6949a6d30d9d532b1f95f3608b73654b5062e7f0
chokodonut <ec2-user@ip-172-31-8-21.ap-northeast-1.compute.internal>
2023-01-19 13:07:56 +0000

    destroy 完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_pash(book.id)
+    redirect_to book_path(book.id)
   end
   
   def create
@@ -24,6 +24,12 @@ class BooksController < ApplicationController
     redirect_to '/books'
     
   end  
+    
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
   
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books,only: [:create, :index, :show, :destroy, :edit, :update]
   root to: 'homes#top'
-  
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  pastch 'books/:id' => 'books#update', as: 'update_list'
+ 
 end
